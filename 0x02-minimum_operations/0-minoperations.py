efd86a0f9135d2ee2b876632c5a204cbd18928fa
mathjken <ogujohnkennedy@gmail.com>
2023-04-07 01:30:49 +0000

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 '''
-this is a python program to compute the minimum number of processes required
+calculates the minimum amount of processes necessary
 to create a certain number of characters.
+You must imagine that there is only one function
+to copy everything and one to paste.
 '''
 
 
 def minOperations(n):
+    p = 0
 
-    if not isinstance(n, int):
-        return 0
+    if n <= 1:
+        return p
 
-        ops = 0
-        i = 2
-        while (i <= n):
-            if not (n % i):
-                n = int(n / i)
-                ops += i
-                i = 1
-                i += 1
-        return ops
+    for i in range(2, n + 1):
+        while (0 == n % i):
+            p = p + i
+            n = n / i
+            if n < i:
+                break
+    return p
